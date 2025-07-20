abfdc96684b923d7c699119691abab6c45303cb3
oheyek <oheyek@proton.me>
2025-07-20 20:07:39 +0200

    Add a new task update

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ import sys
 import datetime
 
 
-def add(task):
+def add(task) -> None:
+    """
+    Function to add a new task to the tasks.json file.
+    :param task: Task description to be added.
+    """
     tasks = []
     try:
         with open("tasks.json", "r") as file:
             tasks = json.load(file)
     except FileNotFoundError:
         pass
-    finally:
-        id = len(tasks) + 1
-        new_task = {
-            "id": id,
-            "description": task,
-            "status": "todo",
-            "createdAt": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "updatedAt": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-        }
-        tasks.append(new_task)
+    id = max((task["id"] for task in tasks), default=0) + 1
+    new_task = {
+        "id": id,
+        "description": task,
+        "status": "todo",
+        "createdAt": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+        "updatedAt": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+    }
+    tasks.append(new_task)
     with open("tasks.json", "w") as file:
-        pass
-    with open("tasks.json", "a") as file:
         json.dump(tasks, file, indent=4)
         print(f"Task added successfully (ID: {id})")
 
@@ -36,8 +37,16 @@ def delete():
 
 
 def main(operation) -> None:
+    """
+    Main function of a program.
+    :param operation: Operation to be performed.
+    """
     match operation:
         case "add":
+            if len(sys.argv) <= 2:
+                raise ValueError(
+                    "Task description is required for the 'add' operation."
+                )
             task_name = str(sys.argv[2])
             add(task_name)
         case "update":
@@ -45,9 +54,12 @@ def main(operation) -> None:
         case "delete":
             delete()
         case _:
-            raise KeyError("Invalid operation")
+            raise ValueError("Invalid operation")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Error: Missing operation. Usage: python main.py <operation> [args]")
+        sys.exit(1)
     operation = sys.argv[1]
     main(operation)
